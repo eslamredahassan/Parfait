3bdbe8afa9c4edb18564effb9ce4fab52efdf25d
Es lam <freelancer.eslamreda@gmail.com>
2023-10-04 02:26:53 +0000

    Update console.js

--- src/assest/console.js
+++ src/assest/console.js
@@ -3,42 +3,42 @@ const moment = require("moment");
 require("moment-duration-format");
 
 module.exports = {
-   // ---- Colors ---- //
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    underscore: "\x1b[4m",
-    blink: "\x1b[5m",
-    reverse: "\x1b[7m",
-    hidden: "\x1b[8m",
-    
-   // ---- Frontground ---- //
-        black: "\x1b[30m",
-        red: "\x1b[31m",
-        green: "\x1b[32m",
-        yellow: "\x1b[33m",
-        blue: "\x1b[34m",
-        magenta: "\x1b[35m",
-        cyan: "\x1b[36m",
-        white: "\x1b[37m",
-        gray: "\x1b[90m",
-        crimson: "\x1b[38m" // Scarlet
-   // ---- Background ---- //
-        black: "\x1b[40m",
-        red: "\x1b[41m",
-        green: "\x1b[42m",
-        yellow: "\x1b[43m",
-        blue: "\x1b[44m",
-        magenta: "\x1b[45m",
-        cyan: "\x1b[46m",
-        white: "\x1b[47m",
-        gray: "\x1b[100m",
-        crimson: "\x1b[48m"
+  // ---- Colors ---- //
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  dim: "\x1b[2m",
+  underscore: "\x1b[4m",
+  blink: "\x1b[5m",
+  reverse: "\x1b[7m",
+  hidden: "\x1b[8m",
+
+  // ---- Frontground ---- //
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  gray: "\x1b[90m",
+  crimson: "\x1b[38m", // Scarlet
+  // ---- Background ---- //
+  back_black: "\x1b[40m",
+  red: "\x1b[41m",
+  green: "\x1b[42m",
+  yellow: "\x1b[43m",
+  blue: "\x1b[44m",
+  magenta: "\x1b[45m",
+  cyan: "\x1b[46m",
+  white: "\x1b[47m",
+  gray: "\x1b[100m",
+  crimson: "\x1b[48m",
 };
 console.log(
   `\x1b[0m`,
   `\x1b[31m 〢`,
   `\x1b[33m ${moment(Date.now()).format("LT")}`,
   `\x1b[31m Consloe Colors`,
-  `\x1b[32m LOADED`
+  `\x1b[32m LOADED`,
 );
