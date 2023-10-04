8de3fdfc33d0456696350c0d1ab122f2d1f81f2c
Es lam <freelancer.eslamreda@gmail.com>
2023-10-04 20:36:09 +0000

    Update answer_no.js and answer_yes.js

--- src/commands/answer_yes.js
+++ src/commands/answer_yes.js
@@ -30,7 +30,7 @@ module.exports = async (client, config) => {
           console.log(
             `\x1b[31m  〢`,
             `\x1b[33m ${moment(Date.now()).format("lll")}`,
-            `\x1b[34m${interaction.user.username} Answered`,
+            `\x1b[34m ${interaction.user.username} Answered`,
             `\x1b[35m Yes for Requirements`,
           );
 
