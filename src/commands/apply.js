81e3f29941f56a473f61aaca74352945a31bc066
Es lam <freelancer.eslamreda@gmail.com>
2023-10-06 01:22:18 +0000

    Update apply.js and reject.js

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -423,6 +423,7 @@ module.exports = async (client, config) => {
         });
 
         console.log(
+          `\x1b[0m`,
           `\x1b[31m  〢`,
           `\x1b[33m ${moment(Date.now()).format("lll")}`,
           `\x1b[34m Created thread for`,
@@ -438,9 +439,10 @@ module.exports = async (client, config) => {
         .add(config.waitRole)
         .catch(() => console.log("Error Line 3478"));
       console.log(
+        `\x1b[0m`,
         `\x1b[31m  🛠`,
-        `\x1b[30m ${moment(Date.now()).format("lll")}`,
-        `\x1b[33m Sun wannabe role added to`,
+        `\x1b[33m ${moment(Date.now()).format("lll")}`,
+        `\x1b[34m Sun wannabe role added to`,
         `\x1b[34m ${interaction.user.username}`,
       );
       ////----------------------------////
