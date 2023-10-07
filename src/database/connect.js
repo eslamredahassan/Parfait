b6be3a55e86b9f1e0301e4e1467d7f43e68e8ae7
Es lam <freelancer.eslamreda@gmail.com>
2023-10-07 04:02:00 +0000

    Update 3 files

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,16 +1,17 @@
 const { MongoClient } = require("mongodb");
 const moment = require("moment");
 const config = require("../config");
+const chalk = require("chalk");
 
 module.exports = async () => {
   try {
     await MongoClient.connect(config.database);
     console.log(
-      `\x1b[0m`,
-      `\x1b[31m 〢`,
-      `\x1b[33m ${moment(Date.now()).format("LT")}`,
-      `\x1b[31m Mongo Database`,
-      `\x1b[32m CONNECTED`,
+      chalk.reset(),
+      chalk.red(` 〢`),
+      chalk.yellow(` ${moment(Date.now()).format("LT")}`),
+      chalk.red(` Mongo Database`),
+      chalk.green.bold(` CONNECTED`),
     );
   } catch (error) {
     console.error(
