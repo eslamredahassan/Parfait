7a9445983efd0123f3933f8aff9732bd057a5190
Es lam <freelancer.eslamreda@gmail.com>
2023-10-12 09:12:34 +0000

    Update smart_questions.js

--- src/interaction/smart_questions.js
+++ src/interaction/smart_questions.js
@@ -20,8 +20,8 @@ module.exports = async (client, config) => {
         case "#ap_questions": {
           console.log(
             `\x1b[31m 〢`,
-            `\x1b[30m ${moment(Date.now()).format("lll")}`,
-            `\x1b[34m${interaction.user.username} USED`,
+            `\x1b[33m ${moment(Date.now()).format("lll")}`,
+            `\x1b[34m ${interaction.user.username} USED`,
             `\x1b[35m Reply Button`,
           );
           //// Modal application code ///
@@ -48,120 +48,183 @@ module.exports = async (client, config) => {
           );
 
           const filter = (m) => m.user.id === thread.user.id;
-          thread.send({ content: `${questions.q1}` }).then(async (msg) => {
-            await msg.channel
-              .awaitMessages({
-                thread: filter,
-                time: 60000,
-                max: 1,
-                errors: ["time"],
-              })
-              .then((collected) => {
-                const msg1 = collected.first().content;
-                thread
-                  .send({ content: `${questions.q2}` })
-                  .then(async (msg) => {
-                    await msg.channel
-                      .awaitMessages({
-                        thread: filter,
-                        time: 60000,
-                        max: 1,
-                        errors: ["time"],
-                      })
-                      .then((collected) => {
-                        const msg2 = collected.first().content;
-                        thread
-                          .send({ content: `${questions.q3}` })
-                          .then(async (msg) => {
-                            await msg.channel
-                              .awaitMessages({
-                                thread: filter,
-                                time: 60000,
-                                max: 1,
-                                errors: ["time"],
-                              })
-                              .then((collected) => {
-                                const msg3 = collected.first().content;
-                                thread
-                                  .send({ content: `${questions.q4}` })
-                                  .then(async (msg) => {
-                                    await msg.channel
-                                      .awaitMessages({
-                                        thread: filter,
-                                        time: 60000,
-                                        max: 1,
-                                        errors: ["time"],
-                                      })
-                                      .then((collected) => {
-                                        const msg4 = collected.first().content;
-                                        thread
-                                          .send({ content: `${questions.q5}` })
-                                          .then(async (msg) => {
-                                            await msg.channel
-                                              .awaitMessages({
-                                                thread: filter,
-                                                time: 60000,
-                                                max: 1,
-                                                errors: ["time"],
-                                              })
-                                              .then((collected) => {
-                                                const msg5 =
-                                                  collected.first().content;
-                                                thread
-                                                  .send({
-                                                    content: `${questions.q6}`,
-                                                  })
-                                                  .then(async (msg) => {
-                                                    await msg.channel
-                                                      .awaitMessages({
-                                                        thread: filter,
-                                                        time: 60000,
-                                                        max: 1,
-                                                        errors: ["time"],
-                                                      })
-                                                      .then((collected) => {
-                                                        const msg6 =
-                                                          collected.first()
-                                                            .content;
-                                                        thread
-                                                          .send({
-                                                            content: `${questions.q7}`,
-                                                          })
-                                                          .then(async (msg) => {
-                                                            await msg.channel
-                                                              .awaitMessages({
-                                                                thread: filter,
-                                                                time: 60000,
-                                                                max: 1,
-                                                                errors: [
-                                                                  "time",
-                                                                ],
-                                                              })
-                                                              .then(
-                                                                (collected) => {
-                                                                  const msg7 =
-                                                                    collected.first()
-                                                                      .content;
-                                                                  return thread.send(
+          thread
+            .send({ content: `${questions.q1}` })
+            .then(async (msg) => {
+              await msg.channel
+                .awaitMessages({
+                  thread: filter,
+                  time: 60000,
+                  max: 1,
+                  errors: ["time"],
+                })
+                .then((collected) => {
+                  const msg1 = collected.first().content;
+                  thread
+                    .send({ content: `${questions.q2}` })
+                    .then(async (msg) => {
+                      await msg.channel
+                        .awaitMessages({
+                          thread: filter,
+                          time: 60000,
+                          max: 1,
+                          errors: ["time"],
+                        })
+                        .then((collected) => {
+                          const msg2 = collected.first().content;
+                          thread
+                            .send({ content: `${questions.q3}` })
+                            .then(async (msg) => {
+                              await msg.channel
+                                .awaitMessages({
+                                  thread: filter,
+                                  time: 60000,
+                                  max: 1,
+                                  errors: ["time"],
+                                })
+                                .then((collected) => {
+                                  const msg3 = collected.first().content;
+                                  thread
+                                    .send({ content: `${questions.q4}` })
+                                    .then(async (msg) => {
+                                      await msg.channel
+                                        .awaitMessages({
+                                          thread: filter,
+                                          time: 60000,
+                                          max: 1,
+                                          errors: ["time"],
+                                        })
+                                        .then((collected) => {
+                                          const msg4 =
+                                            collected.first().content;
+                                          thread
+                                            .send({
+                                              content: `${questions.q5}`,
+                                            })
+                                            .then(async (msg) => {
+                                              await msg.channel
+                                                .awaitMessages({
+                                                  thread: filter,
+                                                  time: 60000,
+                                                  max: 1,
+                                                  errors: ["time"],
+                                                })
+                                                .then((collected) => {
+                                                  const msg5 =
+                                                    collected.first().content;
+                                                  thread
+                                                    .send({
+                                                      content: `${questions.q6}`,
+                                                    })
+                                                    .then(async (msg) => {
+                                                      await msg.channel
+                                                        .awaitMessages({
+                                                          thread: filter,
+                                                          time: 60000,
+                                                          max: 1,
+                                                          errors: ["time"],
+                                                        })
+                                                        .then((collected) => {
+                                                          const msg6 =
+                                                            collected.first()
+                                                              .content;
+                                                          thread
+                                                            .send({
+                                                              content: `${questions.q7}`,
+                                                            })
+                                                            .then(
+                                                              async (msg) => {
+                                                                await msg.channel
+                                                                  .awaitMessages(
                                                                     {
-                                                                      content: `Thank you, our <@&${config.staffSun}> will manage your tryout time soon`,
+                                                                      thread:
+                                                                        filter,
+                                                                      time: 60000,
+                                                                      max: 1,
+                                                                      errors: [
+                                                                        "time",
+                                                                      ],
+                                                                    },
+                                                                  )
+                                                                  .then(
+                                                                    (
+                                                                      collected,
+                                                                    ) => {
+                                                                      const msg7 =
+                                                                        collected.first()
+                                                                          .content;
+                                                                      return thread.send(
+                                                                        {
+                                                                          content: `Thank you, our <@&${config.staffSun}> will manage your tryout time soon`,
+                                                                        },
+                                                                      );
                                                                     },
                                                                   );
-                                                                },
-                                                              );
-                                                          });
-                                                      });
-                                                  });
-                                              });
-                                          });
-                                      });
-                                  });
-                              });
-                          });
-                      });
-                  });
-              });
-          });
+                                                              },
+                                                            )
+                                                            .catch(
+                                                              async (error) => {
+                                                                await thread.send(
+                                                                  "Time is Over",
+                                                                );
+                                                              },
+                                                            );
+                                                        })
+                                                        .catch(
+                                                          async (error) => {
+                                                            await thread.send(
+                                                              "Time is Over",
+                                                            );
+                                                          },
+                                                        );
+                                                    })
+                                                    .catch(async (error) => {
+                                                      await thread.send(
+                                                        "Time is Over",
+                                                      );
+                                                    });
+                                                })
+                                                .catch(async (error) => {
+                                                  await thread.send(
+                                                    "Time is Over",
+                                                  );
+                                                });
+                                            })
+                                            .catch(async (error) => {
+                                              await thread.send("Time is Over");
+                                            });
+                                        })
+                                        .catch(async (error) => {
+                                          await thread.send("Time is Over");
+                                        });
+                                    })
+                                    .catch(async (error) => {
+                                      await thread.send("Time is Over");
+                                    });
+                                })
+                                .catch(async (error) => {
+                                  await thread.send("Time is Over");
+                                });
+                            })
+                            .catch(async (error) => {
+                              await thread.send("Time is Over");
+                            });
+                        })
+                        .catch(async (error) => {
+                          await thread.send("Time is Over");
+                        });
+                    })
+                    .catch(async (error) => {
+                      await thread.send("Time is Over");
+                    });
+                })
+                .catch(async (error) => {
+                  await thread.send("Time is Over");
+                });
+            })
+            .catch(async (error) => {
+              await thread.send("Time is Over");
+            });
         }
         default:
           break;
